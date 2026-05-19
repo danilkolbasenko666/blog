46e4cebc72f1557c79d593658ad11aeac15fdf83
MyUser <MyUser@mail.com>
2026-05-19 16:26:33 +0500

    13 шаблон

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
-use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\PostController as AdminPostController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +23,13 @@ Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
     Route::get('/', [MainController::class, 'index'])->name('admin.index');
     Route::resource('/categories', CategoryController::class);
     Route::resource('/tags', TagController::class);
-    Route::resource('/posts', PostController::class);
+    Route::resource('/posts', AdminPostController::class);
 });
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+
+Route::get('/article/{id}', [PostController::class, 'show'])->name('posts.single');
 
 
 Route::get('/login', [UserController::class, 'loginForm'])->name('login.create');
